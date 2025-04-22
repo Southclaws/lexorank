4b9dfeafffc3299757da4a03e0db4c010a402168
Barnaby Keene <hello@southcla.ws>
2025-04-22 21:14:37 +0100

    add a benchmark/stresstest

---
+++ ranker_bench_test.go
@@ -0,0 +1,52 @@
+package lexorank_test
+
+import (
+	"math"
+	"testing"
+
+	"github.com/Southclaws/lexorank"
+)
+
+type reorderableNode struct {
+	id   int
+	sort lexorank.Key
+}
+
+func (n *reorderableNode) GetKey() lexorank.Key {
+	return n.sort
+}
+
+func (n *reorderableNode) SetKey(k lexorank.Key) {
+	n.sort = k
+}
+
+func BenchmarkReorderableList_FullSpace(b *testing.B) {
+	const base = 75
+	const precision = 3 // 4 and above = 10+ minute benchmark lol
+	maxItems := int(math.Pow(base, precision))
+
+	b.Log("Max items:", maxItems)
+
+	list := make(lexorank.ReorderableList, maxItems)
+	for i := 0; i < maxItems; i++ {
+		list[i] = &reorderableNode{id: i, sort: lexorank.Key{}}
+	}
+
+	b.Log("Done initialising list:", len(list))
+
+	b.ResetTimer()
+	list.Normalise()
+	b.StopTimer()
+
+	b.Log("Done normalising list:")
+
+	for i := 1; i < len(list); i++ {
+		left := list[i-1].GetKey()
+		right := list[i].GetKey()
+		cmp := left.Compare(right)
+
+		if cmp >= 0 {
+			b.Fatalf("Keys not increasing at %d: left: %s, right: %s", i, left.String(), right.String())
+		}
+	}
+}
